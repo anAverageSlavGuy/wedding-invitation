de62a35861dd92cf39b7ed933150ea158ede281c
Yevgeniy Shavlay <yshavlay@mrs-radar.it>
2022-05-26 10:41:46 +0200

    build

--- src/components/Menu.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Menu() {
-  return (
-    <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><h1>Ma che bel menu abbiamo qui <br/> ( ͡° ͜ʖ ͡°)</h1></div>
-  )
-}
-
-export default Menu
